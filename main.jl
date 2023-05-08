acc8760737d0fc4932d00dd82262929e2ddb279c
1m1 <email@1m1.io>
2023-05-08 12:00:37 -0600

    base working

--- main.jl
+++ main.jl
@@ -1,3 +1,4 @@
+# add LibSndFile PortAudio SampledSignals FileIO Whisper OpenAI
 import LibSndFile
 using PortAudio
 using SampledSignals: s
@@ -7,7 +8,8 @@ using Whisper, LibSndFile, FileIO, SampledSignals
 using OpenAI
 
 function save_audio_from_mic(duration)
-    shure_device = filter(d->startswith(lowercase(d.name), "shure"),PortAudio.devices())[1]
+    # shure_device = filter(d->startswith(lowercase(d.name), "shure"),PortAudio.devices())[1]
+    shure_device = filter(d->startswith(lowercase(d.name), "macbook air microphone"),PortAudio.devices())[1]
 
     stream = PortAudioStream(shure_device, 1, 0)
 
@@ -36,12 +38,13 @@ end
 
 function ask_chatgpt(prompt)
     # secret_key = "PAST_YOUR_SECRETE_KEY_HERE"
+    # model = "davinci"
     model = "gpt-3.5-turbo"
     r = create_chat(
         secret_key, 
         model,
         [
-            Dict("role" => "system", "content"=> "You are a julia coder. Only send back code, with a docstring and no explanations"),
+            Dict("role" => "system", "content"=> "You are a julia coder. Only send back code, with a docstring and no explanations. Do not add using statements."),
             Dict("role" => "user", "content"=> prompt)
         ]
     )
@@ -49,18 +52,48 @@ function ask_chatgpt(prompt)
 end
 
 ####
-
+response = ""
+code = ""
 function ask(duration)
     audio = save_audio_from_mic(duration)
-    text = audio_buffer_to_text(audio)
+    
+    text = lowercase(audio_buffer_to_text(audio))
     print(text)
-    response = ask_chatgpt(text)
+
+    m = match(r"\s*command", text)
+    if m ≠ nothing
+        @show "command", m
+        text = text[m.offset+length("command"):end]
+        @show text
+        print("command: ", text)
+        return eval(Meta.parse(text))
+    end
+
+    @show "not command"
+    global response = ask_chatgpt(text)
+    global code = response[10:end-3]
     print(response)
     response
 end
 
-@time response = ask(10s);
+function run()
+    @show "run code? y/*", code
+    answer = readline()
+    answer ≠ "y" && return
+    # TODO a better (reliable) way to get code
+    eval(Meta.parse(code))
+end
 
-# updated_response = replace(response, "add_four_numbers" => "add_four_numbers_2")
-code = updated_response[10:end-3]
-eval(Meta.parse(code))
+# convenience
+a(duration) = @time ask(duration)
+r = run
+
+#####
+
+# teach the coder ML
+
+# Base.run(`openai tools fine_tunes.prepare_data -f finetune.csv`)
+
+# Base.run(`openai api fine_tunes.create -t finetune.jsonl -m davinci`)
+# Base.run(`export OPENAI_API_KEY=a`)
+# Base.run(`echo $OPENAI_API_KEY`)
