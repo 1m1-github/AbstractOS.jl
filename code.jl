2b03476bac6cde9286162d718557d2a7742e6eea
1m1 <email@1m1.io>
2023-07-27 00:45:11 -0400

    more thots

---
+++ code.jl
@@ -0,0 +1,13 @@
+"""
+dissect my command into an action and an input upon which this action will be applied.
+you should write a function representing that action.
+
+"it" should in general refer to the memory kept in the variable "ans".
+<!-- is this needed? -->
+
+your entire output will only consist of code.
+
+you will write exactly one function, which will have a typed signature.
+
+the name of the function should be as descriptive in human language as needed. 
+"""
