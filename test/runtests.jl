688345e7c7f2afcd501881542cd3f4cb5ad25786
imi <imi@1m1.io>
2025-10-27 11:58:27 -0700

    runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,35 @@
 include("describe.jl")
+
+run("""create some knowledge that adds a pulling mechanism to any html. so given html, add html (js) that will refresh the page (do the GET that brought this page) every n seconds""")
+run("""write a function that adds a pulling mechanism to any html. so given html, add html (js) that will refresh the page (do the GET that brought this page) every n seconds""")
+
+code_string=read(joinpath(OS_ROOT_DIR, "logs", "log-1756783872-output.jl"), String)
+
+function monitor_stdin_with_visible_whitespace()
+           println("Monitoring stdin. Type anything; Ctrl+C to stop.")
+           while true
+               try
+                   c = read(stdin, Char)
+                   if c == ' ' 
+                       print(".")  # Visible space
+                   elseif c == '\n'
+                       println("\\n")  # Visible newline
+                   elseif c == '\t'
+                       print("\\t")  # Visible tab
+                   elseif isspace(c)
+                       print("<space>")  # Generic whitespace
+                   else
+                       print(c)
+                   end
+               catch e
+                   if e isa EOFError || e isa InterruptException
+                       println("
+       Monitoring stopped.")
+                       break
+                   else
+                       rethrow(e)
+                   end
+               end
+           end
+       end
+
