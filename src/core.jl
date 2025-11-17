76cea73d389d89f52f6a89e0c162549dbac56a4f
imi <imi@1m1.io>
2025-11-17 15:14:21 -0800

    user state fixed :struct and :(::) and :(<:) bugs in describe

--- src/core.jl
+++ src/core.jl
@@ -1,23 +1,43 @@
-const YOUR_PURPOSE = "you are an a learning computer operating system"
+const YOUR_PURPOSE = "you are an a learning and truthful computer operating system"
 
 abstract type IODevice end
 abstract type InputDevice <: IODevice end # e.g. microphone, keyboard, camera, touch, ...
 abstract type OutputDevice <: IODevice end # e.g. speaker, screen, AR, VR, touch, ...
 
 struct TaskElement
-    # todo maybe add `who::String`
+    # todo maybe add `who`
+    task_name::Symbol
     input::String
     output::String
     task::Task
 end
-safe = false # true requires confirmation from user before executing code
-lock = ReentrantLock() # enforces single thread on main intelligence
-input_devices = Dict{Symbol,InputDevice}() # name => device with take!(device::InputDevice)::Any implemented
-output_devices = Dict{Symbol,OutputDevice}() # name => device with put!::InputDevice, info...) implemented
-memory = Dict{Symbol,Any}() # ephemeral, name => anything # todo just use new vars added to jvm
-knowledge = Dict{Symbol,String}() # persisted, name => code
-tasks = Dict{Symbol,TaskElement}(:latest_task => TaskElement("", "", Task(0))) # ephemeral, name => input, output, task
-signals = Dict{Symbol,Bool}(:stop_next => false, :intelligence_running => false) # can be used to communicate
+
+using UUIDs
+mutable struct State
+    state_id::UUID
+    safe::Bool # true requires confirmation from user before executing code
+    lock::ReentrantLock # enforces single thread on main intelligence
+    input_devices::Dict{Symbol,InputDevice} # name => device with take!(device::InputDevice)::Any implemented
+    output_devices::Dict{Symbol,OutputDevice} # name => device with put!::InputDevice, info...) implemented
+    memory::Dict{Symbol,Any} # ephemeral, name => anything # todo just use new vars added to jvm
+    knowledge::Dict{Symbol,String} # persisted, name => code
+    tasks::Dict{Symbol,TaskElement} # ephemeral, task_name => input, output, task
+    signals::Dict{Symbol,Bool} # can be used to communicate
+end
+empty_state() = State(
+    UUID(0),
+    false,
+    ReentrantLock(),
+    Dict{Symbol,InputDevice}(),
+    Dict{Symbol,OutputDevice}(),
+    Dict{Symbol,Any}(),
+    Dict{Symbol,String}(),
+    Dict{Symbol,TaskElement}(:latest_task => TaskElement(:latest_task, "", "", Task(() -> nothing))),
+    Dict{Symbol,Bool}(:stop_next => false, :intelligence_running => false),
+)
+const STATES = Dict{UUID,State}( # state_id => State
+    UUID(0) => empty_state()
+)
 
 macro api(args...)
     isempty(args) && return nothing
@@ -27,17 +47,16 @@ end # used to denote parts of `knowledge` that are presented to the `intelligenc
 global OS_ROOT_DIR, OS_SRC_DIR, OS_KNOWLEDGE_DIR # only persist to OS_ROOT_DIR
 include("describe.jl") # contains `describe` for various types
 
-function learn(code_name::Symbol, code::String)
+function learn(state::State, code_name::Symbol, code::String)
     @info "learn", code_name # DEBUG
-    global knowledge
     try
         clean_code = replace(code, "@api " => "")
         code_expr = Meta.parse("begin $clean_code end")
-        code_name ∈ keys(knowledge) && return
-        code ∈ collect(values(knowledge)) && return
+        code_name ∈ keys(state.knowledge) && return
+        code ∈ collect(values(state.knowledge)) && return
         describe.(find_api_macrocalls(code_expr)) # code should be describable
         eval(code_expr)
-        knowledge[code_name] = code
+        state.knowledge[code_name] = code
         write(joinpath(OS_KNOWLEDGE_DIR, "$code_name.jl"), code)
     catch e
         show(e)
@@ -47,91 +66,97 @@ end
 
 # todo @true mode = provable open source, always runs with safe==true
 
-function listen(device::InputDevice)
+function listen(state::State, device::InputDevice)
     @info "listen", typeof(device) # DEBUG
     while true
+        @info "waiting to take!", typeof(device) # DEBUG
         output = take!(device)
+        @info "listen output", output # DEBUG
         isempty(output) && continue
-        @lock lock next(device, output)
+        @lock state.lock next(state, device, output)
     end
 end
 
-function next(who, what_system, what_user, complexity)
+function next(state::State, who, what_system, what_user, complexity)
     @info "next", who, what_system, what_user, complexity # DEBUG
-    global signals
-    if signals[:stop_next]
+    if state.signals[:stop_next]
         @info "next signals[:stop_next]" # DEBUG
-        signals[:stop_next] = false
+        state.signals[:stop_next] = false
         return
     end
     code_string = ""
-    signals[:intelligence_running] = true
+    state.signals[:intelligence_running] = true
+    system_state = describe(state) * "\n" * what_system
+    @info "got system_state", length(string(who) * system_state * what_user) # DEBUG
+    isa(who, IODevice) && (system_state *= "\nThis is direct input from the user, first will run at lower complexity, use it for planning\n")
+
+    input_logfile = file_stream("input.jl") # DEBUG
+    write(input_logfile, "$who\n$system_state\n$what_user") # DEBUG
+    close(input_logfile) # DEBUG
+
     try
-        system_state = describe() * "\n" * what_system
-        @info "got system_state", length(who * system_state * what_user) # DEBUG
-        isa(who, IODevice) && (system_state *= "\nThis is direct input from the user, first will run at lower complexity, use it for planning\n")
-
-        input_logfile = file_stream("input.jl") # DEBUG
-        write(input_logfile, "$who\n$system_state\n$what_user") # DEBUG
-        close(input_logfile) # DEBUG
-
         code_string = intelligence(who, system_state, what_user, complexity) # `next` is the attached intelligence (you), giving us the natural next output information from input information, and the output should be Julia code
-
-        @info "code_string", code_string # DEBUG
-        output_logfile = file_stream("output.jl") # DEBUG
-        write(output_logfile, code_string) # DEBUG
-        close(output_logfile) # DEBUG
+        # code_string = read("/Users/1m1/logs/log-1763417198-output.jl", String) # DEBUG
     catch e
         @error "`intelligence` failed", e
         return
     finally
-        signals[:intelligence_running] = false
+        state.signals[:intelligence_running] = false
     end
+
+    @info "code_string", code_string # DEBUG
+    output_logfile = file_stream("output.jl") # DEBUG
+    write(output_logfile, code_string) # DEBUG
+    close(output_logfile) # DEBUG
 
     code_expression, task_name = nothing, nothing
     try
         code_expression = Meta.parse(join(["begin", code_string, "end"], '\n'))
-        @info "got code_expression" # DEBUG
+        @info "got code_expression", code_expression # DEBUG
         task_name = taskname(code_expression)
         @info "got task_name", task_name # DEBUG
     catch e
         @error "`Meta.parse` or `taskname` failed", e # DEBUG
-        who *= "_parseortasknameerrorrerun"
+        who = string(who) * "_parseortasknameerrorrerun"
         what_system = join([
             "Your code failed with Exception: $e",
             previous_input_output(what_user, code_string)...,
         ])
         what_user = "Fix and retry without making the same mistake again"
-        return next(who, what_system, what_user, complexity)
+        return next(state, who, what_system, what_user, complexity)
     end
 
     code_imports, code_body = separate(code_expression)
-    @info "got separate" # DEBUG
-    if safe && !confirm()
+    @info "got code_imports", code_imports # DEBUG
+    @info "got code_body", code_body # DEBUG
+    if state.safe && !confirm()
         @info "safe && !confirm()" # DEBUG
-        tasks[:latest_task] = TaskElement(what_user, code_string, Task(0)) # to have to access to the suggested code
+        state.tasks[:latest_task] = TaskElement(:latest_task, what_user, code_string, Task(() -> nothing)) # to have to access to the suggested code
         return # 'safe' guaranteed to be settable by the user (via the REPL)
     end
 
-    run_task(who, what_user, complexity, task_name, code_string, code_imports, code_body)
+    run_task(state, who, what_user, complexity, task_name, code_string, code_imports, code_body)
     @info "next done" # DEBUG
 end
-next(who, what_user, complexity) = next(who, "", what_user, complexity)
-next(who, what_user) = next(who, what_user, 0.5)
-next(what_user) = next("user", what_user, 1.0)
-function next()
-    [Threads.@spawn listen(input_devices[device]) for device in keys(input_devices)]
+next(state::State, who, what_user, complexity) = next(state, who, "", what_user, complexity)
+next(state::State, who, what_user) = next(state, who, what_user, 0.5)
+next(state::State, what_user) = next(state, "user", what_user, 1.0)
+function next(state::State, w::Bool=true)
+    [Threads.@spawn listen(state, device) for (_, device) in state.input_devices]
     # block ~ depends where the system is run from
-    wait(Condition())
+    if w
+        wait(Condition())
+    end
 end
 
-function run_task(who, what_user, complexity, task_name::Symbol, code_string::String, code_imports::Expr, code_body::Expr)
+function run_task(state::State, who, what_user::String, complexity, task_name::Symbol, code_string::String, code_imports::Expr, code_body::Expr)
     @info "run_task", who, what_user, complexity, task_name # DEBUG
-    global tasks
-    code_body = add_latest_task_closure(code_body)
-    @info "got add_latest_task_closure" # DEBUG
-    tasks[:latest_task] = tasks[task_name] =
-        TaskElement(what_user, code_string,
+    code_body = add_latest_task_closure(state, code_body)
+    @info "got add_latest_task_closure", code_body # DEBUG
+    inject_state!(state, code_body)
+    @info "did inject_state!", code_body # DEBUG
+    state.tasks[:latest_task] = state.tasks[task_name] =
+        TaskElement(task_name, what_user, code_string,
             Threads.@spawn try
                 eval(code_imports)
                 @info "got eval(code_imports)" # DEBUG
@@ -140,26 +165,52 @@ function run_task(who, what_user, complexity, task_name::Symbol, code_string::St
                 @error "run_task", e # DEBUG
                 e isa InterruptException && return
                 e_string = Base.invokelatest(string, hasfield(typeof(e), :task) ? e.task.exception : e)
-                who *= "_evalcodeerrorrerun"
+                who = string(who) * "_evalcodeerrorrerun"
                 what_system = join([
                     "`tasks[:$task_name]` failed with Exception: $e_string",
-                    previous_input_output(tasks[task_name].input, tasks[task_name].output)...,])
+                    previous_input_output(state.tasks[task_name].input, state.tasks[task_name].output)...,])
                 what_user = "Fix or restart it if appropriate"
-                return next(who, what_system, what_user, complexity)
+                return next(state, who, what_system, what_user, complexity)
             end)
 end
 previous_input_output(in, out) = ["The `input` was\n$in", "Your output code was\n$out"]
 
-add_latest_task_closure(ex) = ex
-function add_latest_task_closure(ex::Expr)
+add_latest_task_closure(::State, ex) = ex
+function add_latest_task_closure(state::State, ex::Expr)
     if ex.head == :(=) && isa(ex.args[1], Expr) && ex.args[1].head == :ref && ex.args[1].args[1] == :tasks && ex.args[1].args[2] == QuoteNode(:latest_task)
         throw("write to `tasks[:latest_task]` not allowed for closure")
     end
-    new_args = [add_latest_task_closure(a) for a in ex.args]
+    new_args = [add_latest_task_closure(state, a) for a in ex.args]
     if ex.head == :ref && length(ex.args) == 2 && ex.args[1] == :tasks && ex.args[2] == QuoteNode(:latest_task)
-        return tasks[:latest_task]
+        return state.tasks[:latest_task]
     end
     Expr(ex.head, new_args...)
+end
+
+"this function allows you to use state fields such as `state_id`, `memory`, etc. without qualification as the correct state is injected (not `state.state_id`, just `state_id`; not `state.memory`, just `memory`; etc.)"
+function inject_state!(state::State, code::Expr)
+    # @info "inject_state!"
+    # @info field
+    for (i, arg) in pairs(code.args)
+        # @info (i, arg)
+        if arg isa Expr
+            # @info "arg isa Expr"
+            inject_state!(state, arg)
+        elseif arg isa QuoteNode
+            # @info "arg isa QuoteNode"
+            if arg.value isa Expr
+                # @info "arg isa QuoteNode and arg.value isa Expr"
+                inject_state!(state, arg)
+            elseif arg.value in fieldnames(State)
+                # @info "arg isa QuoteNode and arg.value == field"
+                arg = QuoteNode(Expr(:., Expr(:ref, :STATES, state.state_id), QuoteNode(arg.value)))
+            end
+        elseif arg in fieldnames(State)
+            # @info "arg == field"
+            code.args[i] = Expr(:., Expr(:ref, :STATES, state.state_id), QuoteNode(arg))
+        end
+    end
+    code
 end
 
 function separate(code::Expr)::Tuple{Expr,Expr}
--- src/describe.jl
+++ src/describe.jl
@@ -1,18 +1,21 @@
-function describe()::String
+describe() = describe(only(values(STATES)))
+function describe(state::State)::String
     join([
             "describe() BEGIN",
             "OS source code BEGIN\n" * read(joinpath(OS_SRC_DIR, "core.jl"), String) * "\nOS source code END",
-            map(describe, [input_devices, output_devices, memory, knowledge, tasks, signals])...,
+            map(describe, [state.input_devices, state.output_devices, state.memory, state.knowledge, state.tasks, state.signals])...,
             "describe() END",
         ], "\n")
 end
+describe(input_devices::Dict{Symbol, InputDevice}) = describe("input_devices", input_devices)
+describe(output_devices::Dict{Symbol, OutputDevice}) = describe("output_devices", output_devices)
+describe(tasks::Dict{Symbol, TaskElement}) = "tasks BEGIN\n" * join(map(name -> describe(tasks[name]), collect(keys(tasks))), '\n') * "\ntasks END"
+describe(memory::Dict{Symbol, Any}) = "memory BEGIN\n" * join(map(name -> ":$name=>$(memory[name])", collect(keys(memory))), '\n') * "\nmemory END"
+describe(knowledge::Dict{Symbol, String}) = "knowledge BEGIN\n" * join(map(code_name -> describe(code_name, knowledge[code_name]), collect(keys(knowledge))), '\n') * "\nknowledge END"
+describe(signals::Dict{Symbol, Bool}) = "signals BEGIN\n" * join(map(name -> ":$name=>$(signals[name])", collect(keys(signals))), ',') * "\nsignals END"
+
 describe(name::String, d::Dict{Symbol, T}) where T = "$name BEGIN\n" * join(map(k -> ":$k", collect(keys(d))), ',') * "\n$name END"
-describe(::Dict{Symbol, InputDevice}) = describe("input_devices", input_devices)
-describe(::Dict{Symbol, OutputDevice}) = describe("output_devices", output_devices)
-describe(::Dict{Symbol, TaskElement}) = describe("tasks", tasks)
-describe(::Dict{Symbol, Any}) = "memory BEGIN\n" * join(map(name -> ":$name=>$(memory[name])", collect(keys(memory))), '\n') * "\nmemory END"
-describe(::Dict{Symbol, String}) = "knowledge BEGIN\n" * join(map(code_name -> describe(code_name, knowledge[code_name]), collect(keys(knowledge))), '\n') * "\nknowledge END"
-describe(::Dict{Symbol, Bool}) = "signals BEGIN\n" * join(map(name -> ":$name=>$(signals[name])", collect(keys(signals))), ',') * "\nsignals END"
+describe(task::TaskElement) = ":$(task.task_name)(istaskstarted:$(istaskstarted(task.task)),istaskdone:$(istaskdone(task.task)),istaskfailed:$(istaskfailed(task.task)))"
 function describe(code_name::Symbol, code::String)
     result = "knowledge[:$code_name] BEGIN"
     code_expr = Meta.parse("begin $code end")
@@ -49,42 +52,39 @@ end
 function describe(expression::Expr)
     description = ""
     first_arg = expression.args[1]
-    if expression.head in [:curly, :string, :abstract, :primitive]
-        # todo `where` for :curly
-        description = string(expression)
-    elseif expression.head == :.
+    if expression.head == :.
         description = describe(first_arg) * "." * describe(expression.args[2])
-    elseif expression.head == :macro
-        description = "@" * describe(first_arg)
-    elseif expression.head == :(...)
-        description = describe(first_arg) * "..."
-    elseif expression.head == :const
-        description = "const " * describe(first_arg)
-    elseif expression.head == :(::)
-        description = describe(first_arg) * "::" * describe(expression.args[2])
-    elseif expression.head == :(<:)
-        description = describe(first_arg) * "<:" * describe(expression.args[2])
-    elseif expression.head == :function
-        description = describe(first_arg)
-    elseif expression.head == :parameters
-        description = join(describe.(expression.args), ',')
-    elseif expression.head == :kw
-        description = describe(first_arg) * "=" * describe(expression.args[2])
-    elseif expression.head == :block
-        args = filter(a -> !isa(a, LineNumberNode), expression.args)
-        description = join(map(a -> describe(a), args), '\n')
-    elseif expression.head == :struct
-        structname = expression.args[2]
-        description = describe(structname)
-        expression_copy = deepcopy(expression)
-        expression_copy.args[end].args = filter(a -> !isa(a, LineNumberNode), expression.args[end])
-        description = string(expression_copy)
     elseif expression.head == :(=)
         description = describe(first_arg)
         second_arg = expression.args[2]
         if !isa(second_arg, Expr) || second_arg.head ≠ :block
             description *= "=" * describe(second_arg)
         end
+    elseif expression.head == :(::)
+        1 < length(expression.args) && ( description = describe(first_arg))
+        description *= "::" * describe(expression.args[end])
+    elseif expression.head == :(<:)
+        1 < length(expression.args) && ( description = describe(first_arg))
+        description *= "<:" * describe(expression.args[end])
+    elseif expression.head == :(...)
+        description = describe(first_arg) * "..."
+    elseif expression.head == :macro
+        description = "@" * describe(first_arg)
+    elseif expression.head == :const
+        description = "const " * describe(first_arg)
+    elseif expression.head == :function
+        description = describe(first_arg)
+    elseif expression.head == :parameters
+        description = join(describe.(expression.args), ',')
+    elseif expression.head == :kw
+        description = describe(first_arg) * "=" * describe(expression.args[2]) # todo is 2 always correct?
+    elseif expression.head == :block
+        args = filter(a -> !isa(a, LineNumberNode), expression.args)
+        description = join(map(a -> describe(a), args), '\n')
+    elseif expression.head == :struct
+        expression_copy = deepcopy(expression)
+        expression_copy.args[end].args = filter(a -> !isa(a, LineNumberNode), expression.args[end].args)
+        description = string(expression_copy)
     elseif expression.head == :call
         named_params, params = filter_returning_both(a -> a isa Expr && a.head == :parameters, expression.args[2:end])
         params_description = join(map(describe, params), ',')
@@ -94,11 +94,14 @@ function describe(expression::Expr)
         description *= ")"
     elseif expression.head == :macrocall
         if first_arg == docstring_macroname
-            docstring = expression.args[3]
+            docstring = expression.args[3] # todo is 3 always correct?
             description = describe(docstring) * "\n"
         end
         macrobody = expression.args[end]
         description *= describe(macrobody)
+    elseif expression.head in [:curly, :string, :abstract, :primitive]
+        # todo `where` for :curly
+        description = string(expression)
     else
         throw("unknown expression.head == $(expression.head)")
     end
